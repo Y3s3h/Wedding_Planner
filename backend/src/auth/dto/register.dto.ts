11465043ee1f16538a1e47698746a868dd32f2ad
Pearl Kayastha <pearl.gkp@gmail.com>
2026-06-27 22:29:42 +0530

    Implemented Prisma setup and user schema

--- backend/src/auth/dto/register.dto.ts
+++ backend/src/auth/dto/register.dto.ts
@@ -1,5 +1,5 @@
 import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
-
+import { ApiProperty } from '@nestjs/swagger';
 export class RegisterDto {
 
   @IsNotEmpty()
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -19,6 +20,16 @@ async function bootstrap() {
       transform: true,
     }),
   );
+  const config = new DocumentBuilder()
+  .setTitle('Wedding Planner API')
+  .setDescription('API Documentation for Wedding Planner')
+  .setVersion('1.0')
+  .addBearerAuth()
+  .build();
+
+const document = SwaggerModule.createDocument(app, config);
+
+SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 
